39381e98208bbbf8c25e27b9898c6b1c0bd11df9
Yest Izit <hi@dmitry.email>
2022-12-24 22:18:55 +0400

    debug: what?

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,6 +2,7 @@ import axios from "axios"
 
 export const createConfig = () => {
   const backendDomain = import.meta.env.VITE_BACKEND_DOMAIN
+  console.log(import.meta.env)
   const backendUrl = import.meta.env.DEV
     ? `http://${backendDomain}`
     : `https://${backendDomain}`
