2f92c643d314aabee14c833ddbc825d14f5ecd86
Yest Izit <hi@dmitry.email>
2022-12-24 22:16:39 +0400

    fix: urgh

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,9 +2,9 @@ import axios from "axios"
 
 export const createConfig = () => {
   const backendDomain = import.meta.env.VITE_BACKEND_DOMAIN
-  const backendUrl = import.meta.env.PROD
-    ? `https://${backendDomain}`
-    : `http://${backendDomain}`
+  const backendUrl = import.meta.env.DEV
+    ? `http://${backendDomain}`
+    : `https://${backendDomain}`
 
   const axiosInstance = axios.create()
   axiosInstance.defaults.baseURL = backendUrl
