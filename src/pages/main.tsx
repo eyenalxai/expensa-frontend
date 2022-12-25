740e5ca2efc57a25aa9cc3cb0cf4a0388bae0006
Yest Izit <hi@dmitry.email>
2022-12-25 18:28:00 +0400

    feat: show spinner instead of nothing

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,5 @@
 import { Logout } from "@components/logout"
+import { Spinner } from "@pages/login"
 import { Outlet } from "@solidjs/router"
 import { createQuery } from "@tanstack/solid-query"
 import { useFetch } from "@utils/fetch"
@@ -21,19 +22,25 @@ export const Main = () => {
 
   return (
     <div class={clsx("flex", "mt-12", "justify-center")}>
-      <Show when={query.isSuccess && query.data} keyed={true}>
-        {(user) => (
-          <div class={clsx("flex", "flex-col", "gap-3", "items-center")}>
-            <div class={clsx("flex", "flex-row", "gap-4", "items-center")}>
-              <h2 class={clsx("text-slate-700", "dark:text-slate-300")}>
-                Hello, {user.username}!{" "}
-              </h2>
-              <Logout />
-            </div>
-            <Outlet />
+      <div class={clsx("flex", "flex-col", "gap-3", "items-center")}>
+        <div class={clsx("flex", "flex-row", "gap-4", "items-center")}>
+          <div class={"h-6"}>
+            <Show
+              when={query.isSuccess && query.data}
+              keyed={true}
+              fallback={<Spinner />}
+            >
+              {(user) => (
+                <h2 class={clsx("text-slate-700", "dark:text-slate-300")}>
+                  Hello, {user.username}
+                </h2>
+              )}
+            </Show>
           </div>
-        )}
-      </Show>
+          <Logout />
+        </div>
+        <Outlet />
+      </div>
     </div>
   )
 }
